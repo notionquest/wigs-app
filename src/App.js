66b3c078e96750311286470a06c55fed9223b3a1
notionquest <notionquest@gmail.com>
2019-12-25 22:17:23 +0000

    route added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import NavBarHome from './components/navbar/navbar';
+import Home from './components/home/home';
+import Contact from './components/contact/contact';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
+
     <div className="App">
-      <NavBarHome/>
+      <BrowserRouter>
+        <NavBarHome />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/contact'>
+            <Contact />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,6 +6,9 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import HomeIcon from '../homeicon/HomeIcon';
 import ContactsIcon from '@material-ui/icons/Contacts';
+import { Link, Switch, Route } from 'react-router-dom'
+import Home from '../home/home';
+import Contact from '../contact/contact';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -24,19 +27,31 @@ export default function NavBarHome() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="fixed" color="white">
+      <AppBar position="fixed" color="default">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <HomeIcon />
+            <Link to="/">
+              <HomeIcon />
+            </Link>
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             Wigs
           </Typography>
           <IconButton edge="end" className={classes.menuButton} color="inherit" aria-label="menu">
-            <ContactsIcon />
+            <Link to="/contact">
+              <ContactsIcon />
+            </Link>
           </IconButton>
         </Toolbar>
       </AppBar>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/contact'>
+          <Contact />
+        </Route>
+      </Switch>
     </div>
   );
 }
