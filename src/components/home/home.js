76884d0b823633d411009b43534934883396e4e8
notionquest <notionquest@gmail.com>
2019-12-27 18:15:12 +0000

    Filters and sizes using row and col

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,49 +1,54 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
-const MainHome = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = () => setIsOpen(!isOpen);
+class MainHome extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      filters: {
+        sizes: ["Small", "Medium", "Large"]
+      }
+    };
+  }
+  render() {
+    return (
+      <div class="container-fluid border">
 
-  return (
-    <div>
-      {/* Home Filter */}
-      <div class="w-25 p-3">
-        <div class="row">
-          <div class="col-2 font-weight-bolder">
-            Filter
+
+        {/* Home Filter */}
+        <div class="col-2 border">
+          <div class="row">
+            <div class="col">
+              <label class="badge badge-primary text-left">Filters</label>
+            </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-3 font-weight-bold">
-            Size
+          <div class="row">
+            <div class="col">
+              <label class="badge badge-secondary">Size</label>
+            </div>
           </div>
+          {this.state.filters.sizes.map(size =>
+            <div class="row">
+              <div class="col">
+                <ul>
+                  <ol>
+                    <input type="checkbox" id={size} />
+                    <label for="colFormLabelSm" className="col-sm-2 col-form-label col-form-label-sm">{size}</label>
+                  </ol>
+                </ul>
+              </div>
+            </div>
+          )}
         </div>
-        <div class="row">
-          <div class="col-3">
-            <input type="checkbox" name="size" id="small" />
-            <label for="small">Small</label>
-          </div>
+
+        {/* Home Body */}
+        <div class="col-10 border">
+          <h2>Home</h2>
         </div>
-        <div class="row">
-          <div class="col-3">
-            <input type="checkbox" name="size" id="medium" />
-            <label for="medium">Medium</label>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-3">
-            <input type="checkbox" name="size" id="large" />
-            <label for="large">Large</label>
-          </div>
-        </div>
+
       </div>
-      {/* Home Body */}
-      <div>
-        <h2>Home</h2>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default MainHome;
