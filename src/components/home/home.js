bd1dadb06662d5d43f0cea955f91be1fba8a1d82
notionquest <notionquest@gmail.com>
2019-12-27 14:37:29 +0000

    bootstrap 4 and navbar

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Home() {
-    return <h2>Home</h2>;
-  }
+const MainHome = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      {/* Home Filter */}
+      <div class="w-25 p-3">
+        <div class="row">
+          <div class="col-2 font-weight-bolder">
+            Filter
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-3 font-weight-bold">
+            Size
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-3">
+            <input type="checkbox" name="size" id="small" />
+            <label for="small">Small</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-3">
+            <input type="checkbox" name="size" id="medium" />
+            <label for="medium">Medium</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-3">
+            <input type="checkbox" name="size" id="large" />
+            <label for="large">Large</label>
+          </div>
+        </div>
+      </div>
+      {/* Home Body */}
+      <div>
+        <h2>Home</h2>
+      </div>
+    </div>
+  );
+}
+
+export default MainHome;
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,57 +1,35 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import HomeIcon from '../homeicon/HomeIcon';
-import ContactsIcon from '@material-ui/icons/Contacts';
-import { Link, Switch, Route } from 'react-router-dom'
-import Home from '../home/home';
-import Contact from '../contact/contact';
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+const NavBarHome = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-export default function NavBarHome() {
-  const classes = useStyles();
+  const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <div className={classes.root}>
-      <AppBar position="fixed" color="default">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <Link to="/">
-              <HomeIcon />
-            </Link>
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Wigs
-          </Typography>
-          <IconButton edge="end" className={classes.menuButton} color="inherit" aria-label="menu">
-            <Link to="/contact">
-              <ContactsIcon />
-            </Link>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route path='/contact'>
-          <Contact />
-        </Route>
-      </Switch>
+    <div class="container-fluid">
+      
+      <Navbar color="#FFFFFF" light expand="md">
+        <NavbarBrand href="/">Wigs</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/contact">Contact</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
     </div>
   );
 }
+
+export default NavBarHome;
