55b15211befa6c918b54e974ce1a9626333026ee
notionquest <notionquest@gmail.com>
2019-12-28 10:27:21 +0000

    footer added

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -11,54 +11,53 @@ class MainHome extends Component {
     };
   }
   render() {
+
     return (
       <div class="container-fluid border">
 
 
-        {/* Home Filter */}
-        <div class="col-2 border">
-          <div class="row">
-            <div class="col">
-              <label class="badge badge-primary text-left">Filters</label>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col">
-              <label class="badge badge-secondary">Size</label>
-            </div>
-          </div>
-          {/* <div class="row"> */}
-            {/* <div class="col-sm-2"> */}
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title">Size</h5>
-                  {this.state.filters.sizes.map(size =>
-/*                     <div class="row">
-                      <div class="col"> */
-                        <ul>
-                          <ol>
-                            <input type="checkbox" id={size} />
-                            <label for="colFormLabelSm" >{size}</label>
-                          </ol>
-                        </ul>
-/*                       </div>
-                    </div> */
-                  )}
-                </div>
+        <div class="row">
+          {/* Home Filter */}
+          <div class="col-2 border d-table">
+            <div class="row">
+              <div class="col bg-primary">
+                <p class="text-white font-weight-bold">Filters</p>
               </div>
-            {/* </div> */}
-         {/*  </div> */}
-
-
-          {/* Home Body */}
-          <div class="col-10 border">
-            <h2>Home</h2>
+            </div>
+            <div class="card">
+              <div class="card-header col-xs-12 col-md-12 bg-info text-white d-flex">
+                <p>Size</p>
+              </div>
+              <div class="card-body">
+                {this.state.filters.sizes.map(size =>
+                  <ul>
+                    <ol>
+                      <input type="checkbox" id={size} />
+                      <label for="colFormLabelSm" >{size}</label>
+                    </ol>
+                  </ul>
+                )}
+              </div>
+            </div>
           </div>
+          <div>
+            {/* Home Body */}
+            <div class="col-10 border d-table">
+              <h2>Home</h2>
+            </div>
+          </div>
+        </div>
 
+        <div>
+          <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
+            <div class="container text-center">
+              <small>Copyright &copy; Wigs Website</small>
+            </div>
+          </footer>
         </div>
-        </div>
-        );
-      }
-    }
-    
+      </div>
+    );
+  }
+}
+
 export default MainHome;
