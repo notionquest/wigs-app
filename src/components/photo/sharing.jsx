e751cda3db7fb1c3d5a6b152fb5945e18eb67052
notionquest <notionquest@gmail.com>
2019-12-29 18:07:36 +0000

    facebook and mail share options added

--- src/components/photo/sharing.jsx
+++ src/components/photo/sharing.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import {Twitter} from 'react-social-sharing'
+import { Twitter, Facebook, Mail } from 'react-social-sharing'
 
 class Sharing extends Component {
     constructor(props) {
@@ -7,9 +7,15 @@ class Sharing extends Component {
         this.state = {
             wigInfo: props.wigInfo
         };
-      }
+    }
     render() {
-        return (<div><Twitter solid small message={this.state.wigInfo.title} link={process.env.REACT_APP_HOME_URL}/></div>);
+        return (
+            <div>
+                <Twitter solid small message={this.state.wigInfo.title} link={process.env.REACT_APP_HOME_URL} />
+                <Facebook solid small message={this.state.wigInfo.title} link={process.env.REACT_APP_HOME_URL} />
+                <Mail solidcircle small message={this.state.wigInfo.title} link={process.env.REACT_APP_HOME_URL}/>
+            </div>
+        );
     }
 }
 
