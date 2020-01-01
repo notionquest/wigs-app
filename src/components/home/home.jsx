0e97511f3defdc4edce7f51c9d27c8f3363907a9
notionquest <notionquest@gmail.com>
2020-01-01 16:12:50 +0000

    Size card formatting

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Photos from '../photo/photos';
-import { Badge, Card, CardBody, Button, CardHeader, CardTitle, CardText } from 'reactstrap';
+import { Badge, Card, CardBody } from 'reactstrap';
 
 
 class MainHome extends Component {
@@ -23,24 +23,8 @@ class MainHome extends Component {
                 <h4><Badge>Filters</Badge></h4>
               </div>
             </div>
-            {/*             <div className="card">
-              <div className="col">
-                <h5><Badge color="primary">Size</Badge></h5>
-              </div>
-              <div className="card-body">
-                {this.state.filters.sizes.map(size =>
-                  <div className="card-text" key={size}>
-                    <div className="row">
-                      <input type="checkbox" id={size} />
-                      <label htmlFor={size} className="text-dark font-weight-normal"><small>{size}</small></label>
-                    </div>
-                  </div>
-                )}
-              </div>
-            </div> */}
             <div className="row">
               <Card className="border-0">
-                <CardTitle >
                   <div className="row">
                     <div className="col-1">
                     </div>
@@ -48,10 +32,9 @@ class MainHome extends Component {
                       <h5><Badge color="primary">Size</Badge></h5>
                     </div>
                   </div>
-                </CardTitle>
                 <CardBody>
-                {this.state.filters.sizes.map(size =>
-                  <div className="row">
+                {this.state.filters.sizes.map((size, index) =>
+                  <div className="row" key={index}>
                     <div className="col-2"></div>
                     <div>
                       <input type="checkbox" id={size} />
