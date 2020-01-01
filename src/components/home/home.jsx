a8b352e396e59a405417017113bfd50768b9b5ef
notionquest <notionquest@gmail.com>
2020-01-01 15:58:44 +0000

    size formatted

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Photos from '../photo/photos';
-import { Badge } from 'reactstrap';
+import { Badge, Card, CardBody, Button, CardHeader, CardTitle, CardText } from 'reactstrap';
 
 
 class MainHome extends Component {
@@ -20,10 +20,10 @@ class MainHome extends Component {
           <div className="col-2">
             <div className="row">
               <div className="col">
-                <h4><Badge><p className="d-inline text-white font-weight-bold">Filters</p></Badge></h4>
+                <h4><Badge>Filters</Badge></h4>
               </div>
             </div>
-            <div className="card">
+            {/*             <div className="card">
               <div className="col">
                 <h5><Badge color="primary">Size</Badge></h5>
               </div>
@@ -37,11 +37,35 @@ class MainHome extends Component {
                   </div>
                 )}
               </div>
+            </div> */}
+            <div className="row">
+              <Card className="border-0">
+                <CardTitle >
+                  <div className="row">
+                    <div className="col-1">
+                    </div>
+                    <div className="col-4">
+                      <h5><Badge color="primary">Size</Badge></h5>
+                    </div>
+                  </div>
+                </CardTitle>
+                <CardBody>
+                {this.state.filters.sizes.map(size =>
+                  <div className="row">
+                    <div className="col-2"></div>
+                    <div>
+                      <input type="checkbox" id={size} />
+                      <label htmlFor={size} className="text-dark font-weight-normal"><small>{size}</small></label>
+                    </div>
+                  </div>
+                )}
+                </CardBody>
+              </Card>
             </div>
           </div>
           <div className="col-10">
             {/* Home Body */}
-              <Photos />            
+            <Photos />
           </div>
         </div>
       </div>
